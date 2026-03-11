815ff2e2af501bca4c798e8ee847270847d23a85
derekhuit-afk <derekhuit@gmail.com>
2026-03-10 21:32:12 -0800

    chore: add ElevenLabs voice env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    ELEVENLABS_VOICE_ID: process.env.ELEVENLABS_VOICE_ID || "EXAVITQu4vr4xnSDxMaL",
+  },
+}
 module.exports = nextConfig
